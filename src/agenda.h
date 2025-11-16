e70f97a0b9ce1af1abdd032ac971ea7f84ba7b56
scitlaliitzel-cloud <scitlaliitzel@gmail.com>
2025-11-16 13:27:22 -0600

    Update agenda.h

--- src/agenda.h
+++ src/agenda.h
@@ -1,39 +1,61 @@
 #ifndef __AGENDA_H__
 #define __AGENDA_H__
 
+#include <stdio.h>
+#include <string.h>
+
 #define MAX_CONTACTOS 100
 
+// Tipos de teléfono
 enum TipoTelefono {CASA, MOVIL, OFICINA, OTRO};
-enum Mes{ENERO, FEBERO, MARZO, ABRIL, MAYO, JUNIO, JULIO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE};
 
-struct Persona{
+// Meses del año
+enum Mes {
+    ENERO, FEBERO, MARZO, ABRIL, MAYO, JUNIO, JULIO,
+    SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE
+};
+
+// Estructura del contacto
+struct Persona {
     char nombre[30];
-    // apellido
-    // mes de nacimiento
-    // dia de nacimiento
-    // tipo contacto
-    // numero de telefono
-    // tipo de numero
+    char apellido[30];
+    int dia;
+    enum Mes mes;
+    char telefono[20];
+    enum TipoTelefono tipo;
 };
 
 typedef struct Persona Contacto;
 
-typedef struct Agenda{
+// Estructura Agenda
+typedef struct Agenda {
     Contacto contactos[MAX_CONTACTOS];
-    int num_contactos; //Lleva la cuenta de cuantos contactos están en la agenda
+    int num_contactos;
 } Agenda;
 
+// PROTOTIPOS DE FUNCIONES
 
 void iniciar_agenda(Agenda *agenda);
+void inicializar_agenda(Agenda *agenda);  // algunos tests usan este nombre también
+
 void agregar_contacto(Agenda *agenda, Contacto c);
+
 void imprimir_agenda(Agenda agenda);
+void imprimir_contactos(Agenda agenda);   // requerido por test_mostrar_contactos
+
 int buscar_contacto(Agenda *agenda, char *nombre);
 int buscar_contacto_x_telefono(Agenda *agenda, char telefono[]);
+
 void ordenar_contactos(Agenda *agenda);
 void ordenar_contactos_inv(Agenda *agenda);
-void mostrar_contacto(Contacto);
-void leer_contacto(Contacto *c);
-void cargar_contactos(char *filename);
-void guardar_contactos(char *filename);
 
-#endif // __AGENDA_H_
+void mostrar_contacto(Contacto c);
+void leer_contacto(Contacto *c);
+
+void cargar_contactos(char *filename, Agenda *agenda);
+void leer_agenda(Agenda *agenda, char *filename); // requerido por test_leer
+
+void guardar_contactos(char *filename, Agenda agenda);
+void guardar_agenda(char *filename, Agenda agenda); // requerido por test_guardar
+
+#endif // __AGENDA_H__
