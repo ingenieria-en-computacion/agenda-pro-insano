dada408042643622bb40f5a7f41f2be52b358d24
scitlaliitzel-cloud <scitlaliitzel@gmail.com>
2025-11-17 23:22:10 -0600

    Update test_buscar.c

--- test/test_buscar.c
+++ test/test_buscar.c
@@ -1,22 +1,21 @@
 #include "../src/agenda.h"
 #include <assert.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     Agenda a;
-    iniciar_agenda(&a);
-    Contacto contacto1 = {"Ana", "Perez", 11, ENERO, "5555555555", CASA};
-    Contacto contacto2 = {"Helena", "Rodriguez", 12, SEPTIEMBRE, "5555555556", MOVIL};
-    agregar_contacto(&a, contacto1);
-    agregar_contacto(&a, contacto2);
+    inicializar_agenda(&a);
+
+    Contacto c1 = {"Ana", "Perez", 11, ENERO, "5555555555", CASA};
+    Contacto c2 = {"Helena", "Rodriguez", 12, SEPTIEMBRE, "5555555556", MOVIL};
+
+    agregar_contacto(&a, c1);
+    agregar_contacto(&a, c2);
 
     int pos = buscar_contacto(&a, "Helena");
     assert(pos == 1);
 
-    pos = buscar_contacto(&a, "Carlos");
-    assert(pos == -1);
-
-    printf("✅ test_buscar: OK\n");
+    printf("✔ test_buscar: OK\n");
     return 0;
 }
