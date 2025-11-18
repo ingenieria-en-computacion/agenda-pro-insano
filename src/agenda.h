f842ea12980769787556ad4456129617e4463d33
scitlaliitzel-cloud <scitlaliitzel@gmail.com>
2025-11-17 22:40:51 -0600

    Update agenda.h

--- src/agenda.h
+++ src/agenda.h
@@ -1,49 +1,42 @@
 #ifndef __AGENDA_H__
 #define __AGENDA_H__
 
-#include <stdio.h>
-#include <string.h>
-
 #define MAX_CONTACTOS 100
 
 enum TipoTelefono {CASA, MOVIL, OFICINA, OTRO};
+enum Mes{ENERO, FEBERO, MARZO, ABRIL, MAYO, JUNIO, JULIO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE};
 
-enum Mes {
-    ENERO, FEBRERO, MARZO, ABRIL, MAYO, JUNIO,
-    JULIO, AGOSTO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE
+struct Persona{
+    char nombre[30];
+    // apellido
+    // mes de nacimiento
+    // dia de nacimiento
+    // tipo contacto
+    // numero de telefono
+    // tipo de numero
 };
 
-typedef struct {
-    char nombre[30];
-    char apellido[30];
-    int dia;
-    enum Mes mes;
-    char telefono[20];
-    enum TipoTelefono tipo;
-} Contacto;
+typedef struct Persona Contacto;
 
-typedef struct {
+typedef struct Agenda{
     Contacto contactos[MAX_CONTACTOS];
-    int num_contactos;
+    int num_contactos; //Lleva la cuenta de cuantos contactos están en la agenda
 } Agenda;
 
+
 void iniciar_agenda(Agenda *agenda);
-void inicializar_agenda(Agenda *agenda);
-
 void agregar_contacto(Agenda *agenda, Contacto c);
-
+void imprimir_agenda(Agenda agenda);
 int buscar_contacto(Agenda *agenda, char *nombre);
 int buscar_contacto_x_telefono(Agenda *agenda, char telefono[]);
-
 void ordenar_contactos(Agenda *agenda);
 void ordenar_contactos_inv(Agenda *agenda);
+void mostrar_contacto(Contacto);
+void leer_contacto(Contacto *c);
+void cargar_contactos(char *filename);
+void guardar_contactos(char *filename);
 
-void mostrar_contacto(Contacto c);
-void imprimir_agenda(Agenda agenda);
-void imprimir_contactos(Agenda agenda);
-
-void cargar_contactos(char *filename, Agenda *agenda);
-void leer_agenda(Agenda *agenda, char *filename);
+#endif // __AGENDA_H_
 
 void guardar_contactos(char *filename, Agenda agenda);
 void guardar_agenda(char *filename, Agenda agenda);
