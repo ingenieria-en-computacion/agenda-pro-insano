ef910479dcfea1da8abb97fd532dd95ca1a1d3fc
scitlaliitzel-cloud <scitlaliitzel@gmail.com>
2025-11-17 23:23:08 -0600

    Update test_ordenar.c

--- test/test_ordenar.c
+++ test/test_ordenar.c
@@ -7,20 +7,20 @@ int main() {
     Agenda a;
     inicializar_agenda(&a);
 
-    Contacto contacto1 = {"Ana", "Perez", 11, ENERO, "5555555555", CASA};
-    Contacto contacto2 = {"Helena", "Rodriguez", 12, SEPTIEMBRE, "5555555556", MOVIL};
-    Contacto contacto3 = {"Carlos", "Rodriguez", 12, SEPTIEMBRE, "5555555556", MOVIL};
+    Contacto c1 = {"Ana", "Perez", 11, ENERO, "5555555555", CASA};
+    Contacto c2 = {"Helena", "Rodriguez", 12, SEPTIEMBRE, "5555555556", MOVIL};
+    Contacto c3 = {"Carlos", "Rodriguez", 12, SEPTIEMBRE, "5555555556", MOVIL};
 
-    agregar_contacto(&a, contacto1);
-    agregar_contacto(&a, contacto2);
-    agregar_contacto(&a, contacto3);
+    agregar_contacto(&a, c1);
+    agregar_contacto(&a, c2);
+    agregar_contacto(&a, c3);
 
     ordenar_contactos_inv(&a);
 
-    assert(strcmp(a.contactos[0].nombre, "Carlos") == 0);
-    assert(strcmp(a.contactos[1].nombre, "Helena") == 0);
+    assert(strcmp(a.contactos[0].nombre, "Helena") == 0);
+    assert(strcmp(a.contactos[1].nombre, "Carlos") == 0);
     assert(strcmp(a.contactos[2].nombre, "Ana") == 0);
 
-    printf(" test_ordenar: OK\n");
+    printf("✔ test_ordenar: OK\n");
     return 0;
 }
