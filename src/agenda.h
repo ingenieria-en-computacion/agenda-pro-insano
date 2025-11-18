9abdf24d0076574bab589eedbf0bba9d9a0729fe
scitlaliitzel-cloud <scitlaliitzel@gmail.com>
2025-11-17 23:12:02 -0600

    Update agenda.h

--- src/agenda.h
+++ src/agenda.h
@@ -1,44 +1,43 @@
 #ifndef __AGENDA_H__
 #define __AGENDA_H__
 
+#include <stdio.h>
+#include <string.h>
+
 #define MAX_CONTACTOS 100
 
 enum TipoTelefono {CASA, MOVIL, OFICINA, OTRO};
-enum Mes{ENERO, FEBERO, MARZO, ABRIL, MAYO, JUNIO, JULIO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE};
 
-struct Persona{
-    char nombre[30];
-    // apellido
-    // mes de nacimiento
-    // dia de nacimiento
-    // tipo contacto
-    // numero de telefono
-    // tipo de numero
+enum Mes {
+    ENERO, FEBRERO, MARZO, ABRIL, MAYO, JUNIO,
+    JULIO, AGOSTO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE
 };
 
-typedef struct Persona Contacto;
+typedef struct {
+    char nombre[30];
+    char apellido[30];
+    int dia;
+    enum Mes mes;
+    char telefono[20];
+    enum TipoTelefono tipo;
+} Contacto;
 
-typedef struct Agenda{
+typedef struct {
     Contacto contactos[MAX_CONTACTOS];
-    int num_contactos; //Lleva la cuenta de cuantos contactos están en la agenda
+    int num_contactos;
 } Agenda;
 
-
-void iniciar_agenda(Agenda *agenda);
+void inicializar_agenda(Agenda *agenda);
 void agregar_contacto(Agenda *agenda, Contacto c);
-void imprimir_agenda(Agenda agenda);
 int buscar_contacto(Agenda *agenda, char *nombre);
 int buscar_contacto_x_telefono(Agenda *agenda, char telefono[]);
-void ordenar_contactos(Agenda *agenda);
 void ordenar_contactos_inv(Agenda *agenda);
-void mostrar_contacto(Contacto);
-void leer_contacto(Contacto *c);
-void cargar_contactos(char *filename);
-void guardar_contactos(char *filename);
+void ordenar_contactos(Agenda *agenda);
 
-#endif // __AGENDA_H_
+void imprimir_contactos(Agenda agenda);
 
-void guardar_contactos(char *filename, Agenda agenda);
+void cargar_contactos(char *filename, Agenda *agenda);
 void guardar_agenda(char *filename, Agenda agenda);
 
 #endif
+
