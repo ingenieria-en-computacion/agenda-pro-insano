6073baeabc26d8897e9be6f5d25899a8aa5c3d63
scitlaliitzel-cloud <scitlaliitzel@gmail.com>
2025-11-17 23:21:55 -0600

    Update test_agregar.c

--- test/test_agregar.c
+++ test/test_agregar.c
@@ -1,14 +1,16 @@
-// tests/test_agregar.c
 #include "../src/agenda.h"
 #include <assert.h>
+#include <string.h>
+#include <stdio.h>
 
 int main() {
     Agenda a;
-    iniciar_agenda(&a);
-    Contacto contacto1 = {"Ana", "Perez", 11, ENERO, "5555555555", CASA};    
-    agregar_contacto(&a, contacto1);    
-    assert(a.num_contactos == 1);
+    inicializar_agenda(&a);
+
+    Contacto c = {"Ana", "Perez", 11, ENERO, "5555555555", CASA};
+    agregar_contacto(&a, c);
+
     assert(strcmp(a.contactos[0].nombre, "Ana") == 0);
-    printf("Prueba agregar_contacto: OK\n");
+    printf("✔ test_agregar: OK\n");
     return 0;
 }
