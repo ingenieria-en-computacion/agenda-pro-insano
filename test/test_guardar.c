d270ecafd9967893e4dfd59d6e59213c7367abac
scitlaliitzel-cloud <scitlaliitzel@gmail.com>
2025-11-17 23:22:25 -0600

    Update test_guardar.c

--- test/test_guardar.c
+++ test/test_guardar.c
@@ -1,26 +1,21 @@
 #include "../src/agenda.h"
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     Agenda a;
-    iniciar_agenda(&a);
-    Contacto contacto1 = {"Ana", "Perez", 11, ENERO, "5555555555", CASA};
-    Contacto contacto2 = {"Helena", "Rodriguez", 12, SEPTIEMBRE, "5555555556", MOVIL};
-    agregar_contacto(&a, contacto1);
-    agregar_contacto(&a, contacto2);
+    inicializar_agenda(&a);
 
-    guardar_agenda("data/test_guardar.txt", a);
+    Contacto c1 = {"Ana", "Perez", 11, ENERO, "5555555555", CASA};
+    Contacto c2 = {"Helena", "Rodriguez", 12, SEPTIEMBRE, "5555555556", MOVIL};
 
-    FILE *f = fopen("data/test_guardar.txt", "r");
-    assert(f != NULL);
+    agregar_contacto(&a, c1);
+    agregar_contacto(&a, c2);
 
-    char linea[256];
-    int count = 0;
-    while (fgets(linea, 256, f)) count++;
-    fclose(f);
+    guardar_agenda("../data/test_guardar.txt", a);
 
-    assert(count == 2);
-    printf("✅ test_guardar: OK\n");
+    printf("✔ test_guardar: OK\n");
     return 0;
 }
+
