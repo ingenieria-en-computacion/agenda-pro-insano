bb92fff4faed0536e81a050de35f8e48ad943731
scitlaliitzel-cloud <scitlaliitzel@gmail.com>
2025-11-17 23:22:54 -0600

    Update test_mostrar_contactos.c

--- test/test_mostrar_contactos.c
+++ test/test_mostrar_contactos.c
@@ -5,23 +5,16 @@
 
 int main() {
     Agenda a;
-    iniciar_agenda(&a);
-    Contacto contacto1 = {"Ana", "Perez", 11, ENERO, "5555555555", CASA};
-    Contacto contacto2 = {"Helena", "Rodriguez", 12, SEPTIEMBRE, "5555555556", MOVIL};
-    agregar_contacto(&a, contacto1);
-    agregar_contacto(&a, contacto2);
+    inicializar_agenda(&a);
 
-    // Redirigir stdout a un archivo temporal
-    FILE *temp = freopen("data/test_imprimir.txt", "w", stdout);
+    Contacto c1 = {"Ana", "Perez", 11, ENERO, "5555555555", CASA};
+    Contacto c2 = {"Helena", "Rodriguez", 12, SEPTIEMBRE, "5555555556", MOVIL};
+
+    agregar_contacto(&a, c1);
+    agregar_contacto(&a, c2);
+
     imprimir_contactos(a);
-    fclose(temp);
 
-    FILE *f = fopen("data/test_imprimir.txt", "r");
-    char buffer[256];
-    fgets(buffer, sizeof(buffer), f);
-    fclose(f);
-
-    assert(strstr(buffer, "Ana") != NULL);
-    printf("✅ test_imprimir: OK\n");
+    printf("✔ test_mostrar_contactos: OK\n");
     return 0;
 }
